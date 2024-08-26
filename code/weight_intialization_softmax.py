942706b7653dd6c47fd2854ef6050a63e1ab8556
leengsmile <leengsmile@126.com>
2024-08-26 17:17:42 +0800

    rename files

--- code/weight_intialization_softmax.py
+++ code/weight_intialization_softmax.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.special import expit as sigmoid
 from torch.utils.data import DataLoader, Dataset, random_split
-from torchmetrics.classification import BinaryAUROC
 
 
 np.random.seed(0)
@@ -20,18 +19,6 @@ def gen_samples(num_samples: int, num_features: int, num_classes: int) -> tuple[
                                n_classes=num_classes)
     X = torch.from_numpy(X).to(torch.float32)
     y = torch.from_numpy(y).to(torch.long)
-    # print(f'type of X: {X.dtype}[{X.shape}], type of y: {y.dtype}[{y.shape}], positive samples: {y.mean()}')
-    return X, y
-    X = torch.normal(0, 1, size=(num_samples, num_features))
-    w = torch.normal(0, 1, size=(num_features, 1))
-    bias = torch.normal(0, 1, size=(1,))
-    z = X + torch.normal(0, 0.1, size=X.shape)
-    # z = X
-    z = X @ w + bias
-    z = sigmoid(z)
-    y = torch.where(z > 0.7, 1., 0.)
-    # print(f'type of X: {X.dtype}[{X.shape}], type of y: {y.dtype}[{y.shape}]')
-
     return X, y
 
 
@@ -41,6 +28,7 @@ class Net(nn.Module):
         super().__init__()
         self.linear = nn.Linear(input_dim, output_dim)
         if zero_weights:
+            # zero out weights and bias
             self.linear.weight.data.zero_()
             self.linear.bias.data.zero_()
         
@@ -63,11 +51,9 @@ class MyDataset(Dataset):
 
 
 def train(dataset: DataLoader, model: Module, optimizer, criterion, 
-          eval_dataset: DataLoader):
-    for epoch in range(10):
+          eval_dataset: DataLoader, num_epoches: int = 1):
+    for epoch in range(num_epoches):
         running_loss = 0.
-        # train_auc = BinaryAUROC()
-        train_auc = 0.
         for i, (inputs, labels) in enumerate(dataset):
             model.train()
 
@@ -78,28 +64,23 @@ def train(dataset: DataLoader, model: Module, optimizer, criterion,
             optimizer.step()
             
             running_loss += loss.item()
-            # train_auc.update(outputs, labels)
             
             if i % 50 == 0:
                 rate = evaluate(eval_dataset, model, optimizer, criterion)
-                print(f'{epoch = }/{i:02d}, train auc: {train_auc}, train loss: {loss.item():.3f}, '
+                print(f'{epoch = }/{i:02d}, train loss: {loss.item():.3f}, '
                       f'evaluate rate: {rate}')
-                
 
 
 def evaluate(dataset: DataLoader, model: Module, optimizer, criterion):
     model.eval()
     total, correct = 0, 0
     running_loss = 0.
-    # eval_auc = BinaryAUROC()
     with torch.no_grad():
         for inputs, labels in dataset:
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             total += labels.size(0)
             running_loss += loss.item() * labels.size(0)
-            # eval_auc.update(outputs, labels)
-    # eval_auc_score = eval_auc.compute().item()
     return 0., running_loss / total
 
 
@@ -113,17 +94,18 @@ def main():
     dataset = MyDataset(X, y)
     train_dataset, valid_dataset = random_split(dataset, [2./3, 1./3])
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-
     valid_loader = DataLoader(valid_dataset, batch_size=128, shuffle=False)
 
     net = Net(input_dim=num_features, output_dim=num_classes, zero_weights=True)
+    print('before training')
     print(net)
     print(net.linear.weight)
     print(net.linear.bias)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001, )
-    train(train_loader, net, optimizer, criterion, eval_dataset=valid_loader)
+    train(train_loader, net, optimizer, criterion, eval_dataset=valid_loader, num_epoches=20)
+    print('after training')
     print(net)
     print(net.linear.weight)
     print(net.linear.bias)
